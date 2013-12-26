19d329a0c1da6f513ebdd4f934bdde60d995cd32
jmateu <jmateu@10.211.55.3>
2013-12-26 12:02:05 +0100

    #1 Proyecto Inicial Java

---
+++ src/aplicacionjava2/AplicacionJava2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacionjava2;
+
+/**
+ *
+ * @author jmateu
+ */
+public class AplicacionJava2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            System.out.println("Hola");
+    }
+}
